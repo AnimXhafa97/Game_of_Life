90c7037993c6b5ce31cfd5435831c5e31e5a0756
Anim Xhafa <animxhafa@gmail.com>
2020-09-05 16:17:24 -0400

    random initial configuration; working on implementing clickable and glider gun

--- sketch.js
+++ sketch.js
@@ -1,18 +1,7 @@
 
-//define binary values for live and dead cells
-var live = 1;
-var dead = 0;
-
-//dimensions of the 2D array
-var rows = 20;
-var cols = 20;
-
-
-//initialize the world variable to be used globally and locally
-var world;
 
 // create the 2D array serving as the grid
-function makeArray(rows, cols) {
+function makeArray(cols, rows) {
   let arr = new Array(cols);
   for (let i = 0; i < arr.length; i++) {
     arr[i] = new Array(rows);
@@ -20,42 +9,61 @@ function makeArray(rows, cols) {
   return arr; //returns an empty 10 x 10 2D array
 }
 
+//dimensions of the 2D array
+var rows = 100;
+var cols = 100;
+
+
+//initialize the world variable to be used globally and locally
+var world = makeArray(cols, rows);
+
 //populate the grid with random live and dead cells
 function setup() {
-  createCanvas(400, 400);
-  world = makeArray(cols, rows);
+  createCanvas(500, 500);
+
+  //click setup
+
+
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
-      world[i][j] = floor(random(2));
+      //world[i][j] = floor(random(2));
+      world[i][j] = 0;
     }
   }
+
+  button = createButton('Random')
+  button = createButton('Run')
+  button = createButton('Stop')
 }
 
 function draw() {
+  background(0)
+
+  //allow mouseclicks
+
 
   //render grid
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
       if (world[i][j] == 1) {
-        fill(0)
-        stroke(100)
-        rect(width/rows*i, height/cols*j, width/rows, height/cols)
+
+        fill(255)
+        stroke(0)
+        rect(width/rows*j, height/cols*i, width/rows, height/cols)
       }
     }
   }
 
-//rendering new grid every generation and implement rules
-let nextgen = makeArray(rows, cols)
+let nextgen = makeArray(cols, rows)
 
 for (let i = 0; i < cols; i++) {
   for (let j = 0; j < rows; j++) {
     let current = world[i][j]
-    let total = 0
-    let neighbors = countNeighbors(world, i, j)
+    let neighbors = countNeighbors(world, i, j);
 
 //the rules
     if (current == 0 && neighbors == 3) {
-      nextgen[i][j] == 1;
+      nextgen[i][j] = 1;
     } else if (current == 1 && (neighbors < 2 || neighbors > 3)) {
       nextgen[i][j] = 0;
     } else {
@@ -64,12 +72,13 @@ for (let i = 0; i < cols; i++) {
   }
 }
 
-  world = nextgen
+  world = nextgen;
+
 
 }
 
 function countNeighbors(world, x, y) {
-  let total = 0
+  let total = 0;
   for (let i = -1; i<2; i++) {
     for (let j = -1; j < 2; j++) {
       let col = (x + i + cols) % cols;
